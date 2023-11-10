0efecb5c9f222b91280d7be2772fb119ddfa827a
Daniil <Daniildaniil1313@mail.ru>
2023-11-10 12:48:58 +0800

    1)Added menu buttons 2)Inserted movie posters 3)Inserted text fields 4)Applied Drawable to backgrounds 5)Applied CardView for rounded corners and levitation 6)Added functionality with the display of Toast messages at the click of each button

--- app/src/main/java/com/example/films/MainActivity.kt
+++ app/src/main/java/com/example/films/MainActivity.kt
@@ -2,12 +2,34 @@ package com.example.films
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val setOnListenerB1 = findViewById<Button>(R.id.button_1)
+        val setOnListenerB2 = findViewById<Button>(R.id.button_2)
+        val setOnListenerB3 = findViewById<Button>(R.id.button_3)
+        val setOnListenerB4 = findViewById<Button>(R.id.button_4)
+        val setOnListenerB5 = findViewById<Button>(R.id.button_5)
+        setOnListenerB1.setOnClickListener {
+            Toast.makeText(this, R.string.button_1, Toast.LENGTH_SHORT).show()
+        }
+        setOnListenerB2.setOnClickListener {
+            Toast.makeText(this, R.string.button_2, Toast.LENGTH_SHORT).show()
+        }
+        setOnListenerB3.setOnClickListener {
+            Toast.makeText(this, R.string.button_3, Toast.LENGTH_SHORT).show()
+        }
+        setOnListenerB4.setOnClickListener {
+            Toast.makeText(this, R.string.button_4, Toast.LENGTH_SHORT).show()
+        }
+        setOnListenerB5.setOnClickListener {
+            Toast.makeText(this, R.string.button_5, Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
