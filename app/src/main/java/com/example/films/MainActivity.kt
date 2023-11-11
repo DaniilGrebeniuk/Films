ece910e8715b21afc0f17d7d8f933ed3ff388f60
Daniil <Daniildaniil1313@mail.ru>
2023-11-11 17:34:29 +0800

    1)Added several posters to the screen and animate their appearance 2)I made an animation of clicking on the poster

--- app/src/main/java/com/example/films/MainActivity.kt
+++ app/src/main/java/com/example/films/MainActivity.kt
@@ -2,34 +2,35 @@ package com.example.films
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import com.example.films.databinding.ActivityMainBinding
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.MaterialToolbar
 
-@Suppress("DEPRECATION")
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) //initializing the binding class
         setContentView(binding.root)
-
-        binding.topAppbar.setOnMenuItemClickListener() {
+        val annotation = AnimationUtils.loadAnimation(this, R.anim.poster_animation)
+        binding.topAppbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.settings -> {
                     Toast.makeText(this, R.string.Settings, Toast.LENGTH_SHORT).show()
+                    annotation.start()
+
                     true
                 }
                 else -> false
             }
         }
+
         binding.navigationBar.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.favorites -> {
                     Toast.makeText(this, R.string.Favorites, Toast.LENGTH_SHORT).show()
+
                     true
                 }
                 R.id.view_later -> {
