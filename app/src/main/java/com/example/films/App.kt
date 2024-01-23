e9cc11d88dfcbc8a8f6ef7dc3fa6fd4a56986a2e
Daniil <Daniildaniil1313@mail.ru>
2024-01-23 18:16:43 +0800

    Dagger used

--- app/src/main/java/com/example/films/App.kt
+++ app/src/main/java/com/example/films/App.kt
@@ -1,49 +1,24 @@
 package com.example.films
 
 import android.app.Application
-import com.example.films.data.Entity.ApiConstants
 import com.example.films.data.Entity.TmdbApi
 import com.example.films.data.MainRepository
-import com.example.films.domain.Interactor
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
-import java.util.concurrent.TimeUnit
+import com.example.films.di.AppComponent
+import com.example.films.di.DaggerAppComponent
+import dagger.internal.DaggerCollections
 
-class App: Application() {
-    lateinit var repo: MainRepository
-    lateinit var interactor: Interactor
-    lateinit var retrofitService : TmdbApi
 
+class App : Application() {
+    lateinit var dagger: AppComponent
     override fun onCreate() {
         super.onCreate()
-        //Инициализируем экземпляр App, через который будем получать доступ к остальным переменным
         instance = this
-        //Инициализируем репозиторий
-        repo = MainRepository()
+        dagger = DaggerAppComponent.create()
 
-        val okHttpClient = OkHttpClient.Builder()
-            .callTimeout(70,TimeUnit.SECONDS)
-            .readTimeout(70,TimeUnit.SECONDS)
-            .writeTimeout(70,TimeUnit.SECONDS)
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                if (BuildConfig.DEBUG) {
-                    level = HttpLoggingInterceptor.Level.BASIC
-                }
-            })
-            .build()
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(ApiConstants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
-            .build()
-        retrofitService = retrofit.create(TmdbApi::class.java)
-        //Инициализируем интерактор
-        interactor = Interactor(repo,retrofitService)
     }
+
+
 
     companion object {
         //Здесь статически хранится ссылка на экземпляр App
